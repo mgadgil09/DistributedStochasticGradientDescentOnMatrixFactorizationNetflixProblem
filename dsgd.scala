5a78f1159876228d13ed4ae1b2c5e162b48f2771
Madhura <mgadgil09@gmail.com>
2015-11-03 20:52:53 -0500

    algo2 implemented. ata algo 1 rahilay

--- dsgd.scala
+++ dsgd.scala
@@ -1,4 +1,3 @@
-
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
@@ -8,42 +7,21 @@ import org.apache.spark.mllib.linalg._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
 
-object SimpleApp {
+object dsgd1 {
   def main(args: Array[String]) {
     val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Distributed SGD")
     val sc = new SparkContext(conf)
     val file = sc.textFile("input.txt")
-    val userKey = file.map{line => line.split(",")}.map{arr => (arr(0).toInt,(arr(1)+":"+arr(2)))}.sortByKey()
-    val ind = userKey.reduceByKey{(a,b) => a.toString+"--"+b.toString}.sortByKey().zipWithIndex	
-    val user = ind.map{case((k,v),i) => (i.toInt,v.toString())}
-    val user1 = user.map{case(k,v) => (k,v.split("--"))}
-    val user2 = user1.flatMap{case(k,v) => {
-		for(i <- 0 to v.length-1)yield(k,v(i))}}
-    val user3 = user2.map{case(k,v) => (k,v.split(":")(0)+","+v.split(":")(1))}	
-    val movie1 = user3.map{case(k,v) => (v.split(",")(0).toInt,k.toString+","+v.split(",")(1))}.sortByKey()	
-    val movie2 = movie1.reduceByKey{(a,b) => a.toString+"--"+b.toString}.sortByKey().zipWithIndex   
-    val movie3 = movie2.map{case((k,v),i) =>(i.toInt,v.toString())}.map{case(k,v) => (k,v.split("--"))}
-    val movie4 = movie3.flatMap{case(k,v) => {for(i <- 0 to v.length-1)yield(k,v(i))}}
-    val umr = movie4.map{case(k,v) => (v.split(",")(0).toInt,k.toString+","+v.split(",")(1))}
-   
-    val muri = umr.map{case(k,v) => (v.split(",")(0).toInt,k.toString+","+v.split(",")(1))}.zipWithIndex.map{case((k,v),i) => (k.toInt,v.toString+","+i.toString)}
-    val colptr = muri.reduceByKey{(a,b) => {if(a.split(",")(0).toInt < b.split(",")(0).toInt) a else b }}.sortByKey()
-    
-    val colptrNew = colptr.map{case(k,v) => v.split(",")(2)}.collect()
-    val values = umr.map{case(k,v) => v.split(",")(1).toInt}
-    val lastcolptr = values.count().toInt
-    var temp = Array(lastcolptr.toInt)
-    var finalcolptr = colptrNew.map{line => line.toInt}
-    finalcolptr = finalcolptr.union(temp)
-    var rowindices = umr.map{case(k,v) => k.toInt}.collect()
-    var rows = rowindices.sorted.distinct
-    var cols = umr.map{case(k,v) => v.split(",")(0).toInt}.collect().sorted.distinct
-    var rowcount = umr.map{case(k,v) => k}.distinct.count().toInt
-    var colcount = umr.map{case(k,v) => v.split(",")(0).toInt}.distinct.count().toInt
-    var doubleValues = values.map{line => line.toDouble}.collect()
-    var mat = new org.apache.spark.mllib.linalg.SparseMatrix(rowcount,colcount,finalcolptr,rowindices,doubleValues)
-    var Blknum = 2
+//didn't know i had to work on 2000*2000 data
+//now i have to create distributed matrix.
+//creating ratings from my original data
+var ratings = file.map{umr => org.apache.spark.mllib.recommendation.Rating(umr.split(",")(0).toInt,umr.split(",")(1).toInt, umr.split(",")(2).toInt)}
+//now creating distributed co-ordinate matrix
+val distMatrix = new CoordinateMatrix(ratings.map {case org.apache.spark.mllib.recommendation.Rating(user, movie, rating) => MatrixEntry(user, movie, rating)}).toBlockMatrix.toLocalMatrix
+var myMatrix = new org.apache.spark.mllib.linalg.DenseMatrix(distMatrix.numRows,distMatrix.numCols,distMatrix.toArray)
+var breezeMyMatrix = new breeze.linalg.DenseMatrix(distMatrix.numRows,distMatrix.numCols,distMatrix.toArray)
+ var Blknum = 2
     def indexCreation(dummyrows:Array[Int],Blk:Int):Array[Array[Int]] = {
     var B = Blk
     var B1 = B
@@ -67,66 +45,82 @@ object SimpleApp {
 
     return Blockrows
 	}
-    var blockrow = indexCreation(rows,Blknum)
-    var blockcol = indexCreation(cols,Blknum)
+val rowIndices = (for(i <- 0 to myMatrix.numRows-1)yield(i)).toArray   
+val colIndices = (for(i <- 0 to myMatrix.numCols-1)yield(i)).toArray   
+ var blockrow = indexCreation(rowIndices,Blknum)
+    var blockcol = indexCreation(colIndices,Blknum)
 //cartesian products of row and col indices
 var car = sc.parallelize(blockrow).cartesian(sc.parallelize(blockcol))
+var makeStratums = car.map{case(x,y) => stratumMaker(x,y)}
+var stratumRdds = for(i <- makeStratums.collect()) yield {sc.parallelize(i)}
+var stratums = stratumRdds.map{case mentry => new CoordinateMatrix(mentry).toBlockMatrix.toLocalMatrix}
+//initializing W0 and H0
+var F = 3
+var w0 = org.apache.spark.mllib.linalg.DenseMatrix.rand(myMatrix.numRows,F,new java.util.Random)
+var h0 = org.apache.spark.mllib.linalg.DenseMatrix.rand(F,myMatrix.numCols,new java.util.Random)
+//breeze Vij, W0, H0
 
-  //my matrix to RDD[Vector]
-    def matrixToRDD(m: Matrix):RDD[org.apache.spark.mllib.linalg.Vector] = {
-   val columns = m.toArray.grouped(m.numRows)
-   val rows = columns.toSeq.transpose 
-   val vectors = rows.map(row => new org.apache.spark.mllib.linalg.DenseVector(row.toArray))
-   sc.parallelize(vectors)
+var breezeVij = stratums.map{stratum => new breeze.linalg.DenseMatrix(stratum.numRows,stratum.numCols,stratum.toArray)}
+//my matrices in breeze Densematrix format
 
-}
-val distrows = matrixToRDD(mat.toDense)
- val distmat = new RowMatrix(distrows)
-
-//now i have to create distributed matrix.
-//creating ratings from my original data
-var ratings = umr.map{case(user,mr) => org.apache.spark.mllib.recommendation.Rating(user.toInt, mr.split(",")(0).toInt, mr.split(",")(1).toInt)}
-//now creating distributed co-ordinate matrix
-val distMatrix = new CoordinateMatrix(ratings.map {case org.apache.spark.mllib.recommendation.Rating(user, movie, rating) => MatrixEntry(user, movie, rating)})
-var blkMatrix = distMatrix.toBlockMatrix
-var blocks = blkMatrix.blocks
-
-var x = car.map{case(x,y) => x(0)}	
+val breezew0 = new breeze.linalg.DenseMatrix[Double](w0.numRows,w0.numCols,w0.values)
+val breezeh0 = new breeze.linalg.DenseMatrix[Double](h0.numRows,h0.numCols,h0.values)
+//joining stratums with original matrix indices
 //I have my stratums
-def stratumMaker(x:Array[Int],y:Array[Int]):scala.collection.immutable.IndexedSeq[org.apache.spark.mllib.linalg.distributed.MatrixEntry] = {
-	var stratumEntries = for(i <- 0 to x.length-1) yield{
-			for(j <- 0 to y.length-1)yield{
-		i.toString+":"+j.toString+":"+mat.apply(x(i),y(j)).toString
+def stratumMaker(x:Array[Int],y:Array[Int]):breeze.linalg.DenseMatrix[Double] = {
+	var stratumEntries = for(j <- 0 to y.length-1)yield{
+				for(i <- 0 to x.length-1) yield{
+					distMatrix.apply(x(i),y(j))
 						}
 					  }
-
-	var stratumRatings = stratumEntries.flatten.map{case(umr) => org.apache.spark.mllib.recommendation.Rating(umr.split(":")(0).toInt, umr.split(":")(1).toInt, umr.split(":")(2).toDouble.toInt)}
-        var stratumMatrix = stratumRatings.map{case org.apache.spark.mllib.recommendation.Rating(user, movie, rating) => MatrixEntry(user, movie, rating)}
+var stratumMatrix = new breeze.linalg.DenseMatrix[Double](x.length,y.length,stratumEntries.flatten.toArray)
+	//var stratumRatings = stratumEntries.flatten.map{case(umr) => org.apache.spark.mllib.recommendation.Rating(umr.split(":")(0).toInt, umr.split(":")(1).toInt, umr.split(":")(2).toDouble.toInt)}
+        //var stratumMatrix = stratumRatings.map{case org.apache.spark.mllib.recommendation.Rating(user, movie, rating) => MatrixEntry(user, movie, rating)}
 stratumMatrix
       
 }
-var makeStratums = car.map{case(x,y) => stratumMaker(x,y)}
-var stratumRdds = for(i <- makeStratums.collect()) yield {sc.parallelize(i)}
-var stratums:org.apache.spark.mllib.linalg.DenseMatrix = stratumRdds.map{case mentry => new CoordinateMatrix(mentry).toBlockMatrix.toLocalMatrix}
-//initializing W0 and H0
-var F = 3
-var w0 = org.apache.spark.mllib.linalg.DenseMatrix.rand(rowcount,F,new java.util.Random)
-var h0 = org.apache.spark.mllib.linalg.DenseMatrix.rand(F,colcount,new java.util.Random)
-//breeze Vij, W0, H0
-val breezew0 = new DenseMatrix[Double](w0.numRows,w0.numCols,w0.values)
-val breezeh0 = new DenseMatrix[Double](h0.numRows,h0.numCols,h0.values)
-var breezeVij = new org.apache.spark.mllib.linalg.DenseMatrix(stratums(0).numRows,stratums(0).numCols,stratums(0).toArray)
-//joining stratums with original matrix indices
-var vIJ = sc.parallelize(stratums).zipWithIndex.map{case(k,v) => (v,k)}.join(car.zipWithIndex.map{case(k,v) => (v,k)}).map{case(a,(b,c)) => (c,b)}
-vIJ.map{case((i,j),mat) =>
-		var I = i
-                var J = j
-                var Vij = mat
-                		
+def wMatrixMaker(iarr:Array[Int],jarr:Array[Int],wMatrix:breeze.linalg.DenseMatrix[Double]):breeze.linalg.DenseMatrix[Double] ={
+var subWMatrixValues =   for(allcols <-0 to wMatrix.cols-1)yield{
+			  for(wi <- 0 to iarr.length-1)yield{
+	                   		wMatrix.apply(iarr(wi),allcols)
+				}
+			}
+var subMatrix = new breeze.linalg.DenseMatrix[Double](iarr.length,wMatrix.cols,subWMatrixValues.flatten.toArray)
+subMatrix	
+}
 
-	
+def hMatrixMaker(iarr:Array[Int],jarr:Array[Int],hMatrix:breeze.linalg.DenseMatrix[Double]):breeze.linalg.DenseMatrix[Double] ={
+var subWMatrixValues =   for(wj <-0 to jarr.length-1)yield{
+			  for(allrows <- 0 to hMatrix.rows-1)yield{
+	                   		hMatrix.apply(allrows,jarr(wj))
+				}
+			}
+var subMatrix = new breeze.linalg.DenseMatrix[Double](hMatrix.rows,jarr.length,subWMatrixValues.flatten.toArray)
+subMatrix	
+}
+ var zippedBlkRow = sc.parallelize(blockrow).zipWithIndex.map{case(k,v) => (v,k)}
+ var zippedBlkCol = sc.parallelize(blockcol).zipWithIndex.map{case(k,v) => (v,k)}
+ var carWithIndex = zippedBlkRow.cartesian(zippedBlkCol)
+var indexedIndices = carWithIndex.map{case((i, arr1), (j, arr2)) => ((arr1,arr2),(i,j))}
+var indexedStratums = sc.parallelize(breezeVij).zipWithIndex.map{case(k,v) => (v,k)}.join(car.zipWithIndex.map{case(k,v) => (v,k)}).map{case(a,(b,c)) => (c,b)}
+var indices = indexedIndices.filter{case((a,b),(i,j)) => {i == 0 && j == 0}}.map{case(k,v) => k}.collect()(0)
+
+for(x <- 0 to blockrow.size)yield{
+	for(y <- 0 to blockcol.size)yield{
+	   var stratsIndices = for(i <- 0 to blockrow.size-1; j <- 0 to blockcol.size-1
+				if i==j)yield{
+			    var xArr =  indexedIndices.filter{case((a,b),(i1,j1)) => {i1 == i.toInt && j1 == j.toInt}}.map{case((a,b),(c,d)) => a}.collect()(0)
+			    var yArr =  indexedIndices.filter{case((a,b),(i1,j1)) => {i1 == i.toInt && j1 == j.toInt}}.map{case((a,b),(c,d)) => b}.collect()(0)
+			    var VStratum =  stratumMaker(xArr,yArr)
+			    var WStratum = wMatrixMaker(xArr,yArr,breezew0)	  
+	  		    var HStratum = hMatrixMaker(xArr,yArr,breezeh0)
+				(VStratum,WStratum,HStratum)
+	 				   }
 		
 
+    		}
+	}
+             
 
-  }
-}
+
+}}
